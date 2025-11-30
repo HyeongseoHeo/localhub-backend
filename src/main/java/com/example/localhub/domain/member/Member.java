12f237ce34e1f9ac189e8b8f4326d13862593635
HyeongseoHeo <cuevo47@chungbuk.ac.kr>
2025-11-30 13:57:55 +0900

    로그인 수정

--- src/main/java/com/example/localhub/domain/member/Member.java
+++ src/main/java/com/example/localhub/domain/member/Member.java
@@ -1,7 +1,14 @@
 package com.example.localhub.domain.member;
 
+import com.example.localhub.domain.board.Comment;
+import com.example.localhub.domain.board.Post;
+import com.example.localhub.domain.board.PostLike;
+import com.example.localhub.domain.board.PostRating;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -18,7 +25,7 @@ public class Member {
     private String email;
 
     @Column(nullable = false)
-    private String password;  //BCrypy로 암호화
+    private String password;  // BCrypt 암호화
 
     @Column(nullable = false)
     private String nickname;
@@ -28,16 +35,37 @@ public class Member {
     private Role role;
 
     @Column(nullable = false)
-    private boolean manager;   // true = 사업자, false = 일반유저
+    private boolean manager;   // true = 사업자
 
     @Column(nullable = false)
     @Builder.Default
     private boolean cleanbotOn = true; // 클린봇 기능 사용 여부
 
     public void toggleCleanbot() {
-        this.cleanbotOn = !this.cleanbotOn; // 설정 껐다 켰다 할 편의 메서드
+        this.cleanbotOn = !this.cleanbotOn;
     }
 
+    // 👇 [여기부터 추가] 회원이 삭제될 때 같이 삭제될 데이터들 설정
+
+    // 1. 내가 쓴 게시글 삭제
+    @OneToMany(mappedBy = "author", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default // Builder 패턴 쓸 때 리스트 초기화 방지
+    private List<Post> posts = new ArrayList<>();
+
+    // 2. 내가 쓴 댓글 삭제
+    @OneToMany(mappedBy = "author", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<Comment> comments = new ArrayList<>();
+
+    // 3. 내가 누른 좋아요 삭제
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<PostLike> likes = new ArrayList<>();
+
+    // 4. 내가 남긴 별점 삭제
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @Builder.Default
+    private List<PostRating> ratings = new ArrayList<>();
 }
 
 
